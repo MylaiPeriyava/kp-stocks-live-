bea39b8ac5f99232eb0779825d8fddaae86b1fe6
MMPT <sukritaa6@gmail.com>
2026-09-24 17:12:17 +0530

    Update kp_server.py

--- kp_server.py
+++ kp_server.py
@@ -577,5 +577,5 @@ def manage_holdings():
         return jsonify({"error": str(error)}), 500
 
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
