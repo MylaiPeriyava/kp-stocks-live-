10312168a6287ffa93366537a446411944876efd
MMPT <sukritaa6@gmail.com>
2026-09-26 11:14:43 +0530

    Update kp_server.py

--- kp_server.py
+++ kp_server.py
@@ -7,7 +7,7 @@
 # - If you really want "no password" for a user, use an empty string "".
 # - Add new users here only; do not redefine elsewhere.
 USERS = {
-    "KP": "",
+    "KP": "vsk",
     "PK": "suk",
     # Example: "RA": "ra_secret",
     # Example with blank password (not recommended): "GUEST": "",
